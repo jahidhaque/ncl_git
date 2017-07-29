2248514948f20513115afbbb9a0ea304f9a241d5
Jahid haque <haque.jahid@gmail.com>
2017-07-29 22:58:57 +0100

    fix authentication

--- app_client/settings/settings.controller.js
+++ app_client/settings/settings.controller.js
@@ -10,93 +10,94 @@
 		.module('nclhalal')
 		.controller('systemCtrl', systemCtrl);
 
-	systemCtrl.$inject = ['systemservice', '$scope', 'productfile'];
+	systemCtrl.$inject = ['systemservice', '$scope', 'productfile', 'authentication', '$location'];
 
-	function 	systemCtrl(systemservice, $scope, productfile){
+	function 	systemCtrl(systemservice, $scope, productfile, authentication, $location){
 		var 	sysvm		=	this;
 
-		sysvm.active		=	"active";
-		sysvm.activeSettingsProduct 	=	"active-settings-btn";
-		sysvm.activeManageProduct 		=	" ";
-		sysvm.addNewProduct 			=	true;
-		sysvm.manageProductOpr			=	false;
-
-		sysvm.products 		=	{
-			product_name: "",
-			category: "",
-			subcat: "",
-			unit: "",
-			inventoryunit: "",
-			price:"",
-			desc: "",
-			image: ""
-		};
-
-
-		// add new product.
-		sysvm.newProduct 			=	function(){
-			// turn back add product form.
-			sysvm.addNewProduct 	=	true;
-			// turn off manage product area.
-			sysvm.manageProductOpr	=	false;
-
-			// remove active css class.
-			sysvm.activeSettingsProduct 	=	' active-settings-btn';
-			// add new active btn.
+		// check whether user logged in or not.
+		if(authentication.isLoggedIn()){
+			sysvm.active		=	"active";
+			sysvm.activeSettingsProduct 	=	"active-settings-btn";
 			sysvm.activeManageProduct 		=	" ";
-		}
-		
-		sysvm.saveProduct		=	function(){			
+			sysvm.addNewProduct 			=	true;
+			sysvm.manageProductOpr			=	false;
 
-			sysvm.products.image 	=	$scope.product_img;
+			sysvm.products 		=	{
+				product_name: "",
+				category: "",
+				subcat: "",
+				unit: "",
+				inventoryunit: "",
+				price:"",
+				desc: "",
+				image: ""
+			};
 
-			if(!sysvm.products.product_name || !sysvm.products.category || !sysvm.products.subcat
-				|| !sysvm.products.unit || !sysvm.products.inventoryunit || !sysvm.products.price
-				|| !sysvm.products.desc){
-				sysvm.error 	=	true;
-				sysvm.message 	=	"All fields are required. Must not be empty";
+			// add new product.
+			sysvm.newProduct 			=	function(){
+				// turn back add product form.
+				sysvm.addNewProduct 	=	true;
+				// turn off manage product area.
+				sysvm.manageProductOpr	=	false;
+
+				// remove active css class.
+				sysvm.activeSettingsProduct 	=	' active-settings-btn';
+				// add new active btn.
+				sysvm.activeManageProduct 		=	" ";
 			}
-			else{
 
-				productfile
-						.uploadProductFile(sysvm.products.image)
-						.then(function(response){
-							if(response.data.success === false){
-								sysvm.error 		=	true;
-								sysvm.message 		=	response.data.error;
-							}
-							else if(response.data.success 	===		true){
-								//update file name in sysvm.products.
-								sysvm.products.image = response.data.filename;
 
-								// calling system service method.
-								systemservice
-											.createProduct(sysvm.products)
-											.then(function(response){
-												if(response.data.success === true){
-													sysvm.successon 	= 	true;
-													sysvm.error 		= 	false;
-													sysvm.message 		= response.data.message;
-												}
-												else if(response.data.error === true){
-													sysvm.error = true;
-													sysvm.message = response.data.message;
-												}
-											})
-											.catch(function(err){
-												console.log(err);
-											})
+			sysvm.saveProduct		=	function(){			
 
-							}
-						})
-						.catch(function(err){
-							console.log(err);
-						})
+				sysvm.products.image 	=	$scope.product_img;
+
+				if(!sysvm.products.product_name || !sysvm.products.category || !sysvm.products.subcat
+					|| !sysvm.products.unit || !sysvm.products.inventoryunit || !sysvm.products.price
+					|| !sysvm.products.desc){
+					sysvm.error 	=	true;
+					sysvm.message 	=	"All fields are required. Must not be empty";
+				}
+				else{
+
+					productfile
+							.uploadProductFile(sysvm.products.image)
+							.then(function(response){
+								if(response.data.success === false){
+									sysvm.error 		=	true;
+									sysvm.message 		=	response.data.error;
+								}
+								else if(response.data.success 	===		true){
+									//update file name in sysvm.products.
+									sysvm.products.image = response.data.filename;
+
+									// calling system service method.
+									systemservice
+												.createProduct(sysvm.products)
+												.then(function(response){
+													if(response.data.success === true){
+														sysvm.successon 	= 	true;
+														sysvm.error 		= 	false;
+														sysvm.message 		= response.data.message;
+													}
+													else if(response.data.error === true){
+														sysvm.error = true;
+														sysvm.message = response.data.message;
+													}
+												})
+												.catch(function(err){
+													console.log(err);
+												})
+
+								}
+							})
+							.catch(function(err){
+								console.log(err);
+							})
+				}
 			}
-		}
 
-
-		// manage products.
+			// manage products.
 		sysvm.manageProduct 	=	function(){
 
 			// remove active css class.
@@ -141,8 +142,15 @@
 								.catch(function(err){
 									alert(err);
 								})
+					}
 				}
 			}
+
 		}
+		else{
+			// redirect user to login page.
+			$location.path('/login');
+		}
+
 	}
 })();
