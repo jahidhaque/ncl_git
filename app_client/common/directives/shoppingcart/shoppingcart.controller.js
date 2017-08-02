33dac2bf603ce5a36806369db26a1ba6bdf82f7b
Jahid haque <haque.jahid@gmail.com>
2017-08-02 17:41:45 +0100

    updates for comments on product

--- app_client/common/directives/shoppingcart/shoppingcart.controller.js
+++ app_client/common/directives/shoppingcart/shoppingcart.controller.js
@@ -52,71 +52,7 @@
 			
 		}
 
-		/*
-		|----------------------------------------------
-		| make comment and show comment.
-		| @author: jahid haque <jahid.haque@yahoo.com>
-		| @copyright: nclhalal, 2017
-		|----------------------------------------------
-		*/
-		// comment form off.
-		cartvm.commentForm 		=		false;
-
-		cartvm.addComment 		=		function(id){
-			
-			// comment object.
-			cartvm.comment 		=		{
-				product_id: id,
-				message: "",
-				user: authentication.currentUser().name
-			};
-
-			// show the comment form.
-			cartvm.commentForm 	=		true;
-			cartvm.activeCat	=	"active-cat-list";
-
-			cartvm.makeComment	=		function(){
-				if(!cartvm.comment.message){
-					cartvm.commentError	=	true;
-					cartvm.commentError_message	=	"Please add comment!";
-				}
-				else{
-					cartvm.commentError	=	false;
-					
-					// calling method from service.
-					nclCart
-							.addComment(cartvm.comment)
-							.then(function(response){
-								if(response.status 			=== 404){
-									cartvm.commentError		=	true;
-									cartvm.commentError_message	=	response.data.error;
-								}
-								else if(response.status 	=== 200){
-									cartvm.commentError		=	false;
-									cartvm.commentForm 		=	false;
-									cartvm.showComments 	=	true;
-									cartvm.productComments 	=	response.data.comments;
-								}
-							})
-							.catch(function(err){
-								alert(err);
-							})
-				}
-			}
-		}
-
-		/*
-		|----------------------------------------------
-		| function to cancle comment 
-		| @author: jahid haque <jahid.haque@yahoo.com>
-		| @copyright: ncl, 2017
-		|----------------------------------------------
-		*/
-		cartvm.cancleComment	=		function(){
-			cartvm.commentForm 		=		false;
-			// removing active state css class from the list.
-			cartvm.activeCat	=	"";
-		}
+		
 	}
 
 })();
--- app_client/products/singleproducts.controller.js
+++ app_client/products/singleproducts.controller.js
@@ -11,39 +11,111 @@
 		.controller('singleProductCtrl', singleProductCtrl);
 
 	// add dependency.
-	singleProductCtrl.$inject 		=		['systemservice', '$routeParams'];
+	singleProductCtrl.$inject 	=	['systemservice', '$routeParams', 'authentication', 'nclCart'];
 
-	function singleProductCtrl(systemservice, $routeParams){
+	function singleProductCtrl(systemservice, $routeParams, authentication, nclCart){
 
 		var 		sngprovm		=		this;
 
-		sngprovm.productname 			=		$routeParams.productname;
 
-		// empty product object.
-		sngprovm.product 			=		"";
+		// show product.
+		sngprovm.showProduct 		=	function(){
 
-		// calling systemservice method.
-		systemservice
-					.singleProduct(sngprovm.productname)
-					.then(function(response){
-						if(response.data.error 		===		true){
-							sngprovm.error 			=		true;
-							sngprovm.message 		=		response.data.message;
-						}
-						else if(response.data.success 		===		true){
-							sngprovm.error 			=		false;
-							sngprovm.success 		=		true;
-							sngprovm.product 		=	response.data.product;
-							console.log(sngprovm.product);
+			sngprovm.productname 			=		$routeParams.productname;
 
-							// checking product has existing comments.
-							if(sngprovm.product.comments.length > 0){
-								sngprovm.hasComments = true;
+			// empty product object.
+			sngprovm.product 			=		"";
+
+			// calling systemservice method.
+			systemservice
+						.singleProduct(sngprovm.productname)
+						.then(function(response){
+							if(response.data.error 		===		true){
+								sngprovm.error 			=		true;
+								sngprovm.message 		=		response.data.message;
 							}
-						}
-					})
-					.catch(function(err){
-						console.log(err);
-					})
+							else if(response.data.success 		===		true){
+								sngprovm.error 			=		false;
+								sngprovm.success 		=		true;
+								sngprovm.product 		=	response.data.product;
+								
+								// checking product has existing comments.
+								if(sngprovm.product.comments.length > 0){
+									sngprovm.hasComments = true;
+								}
+							}
+						})
+						.catch(function(err){
+							console.log(err);
+						})
+		}
+		/*
+		|----------------------------------------------
+		| make comment and show comment.
+		| @author: jahid haque <jahid.haque@yahoo.com>
+		| @copyright: nclhalal, 2017
+		|----------------------------------------------
+		*/
+		// comment form off.
+		sngprovm.commentForm 		=		false;
+
+		sngprovm.addComment 		=		function(id){
+			
+			// comment object.
+			sngprovm.comment 		=		{
+				product_id: id,
+				message: "",
+				user: authentication.currentUser().name
+			};
+
+			// show the comment form.
+			sngprovm.commentForm 	=		true;
+			sngprovm.activeCat	=	"active-cat-list";
+
+			sngprovm.makeComment	=		function(){
+				if(!sngprovm.comment.message){
+					sngprovm.commentError	=	true;
+					sngprovm.commentError_message	=	"Please add comment!";
+				}
+				else{
+					sngprovm.commentError	=	false;
+					
+					// calling method from service.
+					nclCart
+							.addComment(sngprovm.comment)
+							.then(function(response){
+								if(response.status 			=== 404){
+									sngprovm.commentError		=	true;
+									sngprovm.commentError_message	=	response.data.error;
+								}
+								else if(response.status 	=== 200){
+									sngprovm.commentError		=	false;
+									sngprovm.commentForm 		=	false;
+									sngprovm.showComments 	=	true;
+									sngprovm.productComments 	=	response.data.comments;
+
+									// calling show product function.
+									sngprovm.showProduct();
+								}
+							})
+							.catch(function(err){
+								alert(err);
+							})
+				}
+			}
+		}
+
+		/*
+		|----------------------------------------------
+		| function to cancle comment 
+		| @author: jahid haque <jahid.haque@yahoo.com>
+		| @copyright: ncl, 2017
+		|----------------------------------------------
+		*/
+		sngprovm.cancleComment	=		function(){
+			sngprovm.commentForm 		=		false;
+			// removing active state css class from the list.
+			sngprovm.activeCat	=	"";
+		}
 	}
 })();
